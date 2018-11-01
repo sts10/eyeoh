537d35ff673e7c30df38cdab3ddd90a35620d30a
sts10 <sschlinkert@gmail.com>
2018-10-31 21:47:13 -0400

    remove eyeoh lib file

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,11 @@
-pub mod eyeoh;
+// pub mod eyeoh;
+
+use std::io;
+
+pub fn gets() -> Result<String, String> {
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(_n) => Ok(input.trim().to_string()),
+        Err(error) => Err(format!("Error: {}", error.to_string())),
+    }
+}
