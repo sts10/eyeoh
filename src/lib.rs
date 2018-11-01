68502afac6bdecc22de3ed5eb09751eee75379d5
sts10 <sschlinkert@gmail.com>
2018-10-31 22:45:31 -0400

    uses trim_end_matches to remove trailing newline while also preserving leading and trailing whitespace

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use std::io;
 pub fn gets() -> Result<String, String> {
     let mut input = String::new();
     match io::stdin().read_line(&mut input) {
-        Ok(_n) => Ok(input.trim().to_string()),
+        Ok(_n) => Ok(input.trim_end_matches("\n").to_string()),
         Err(error) => Err(format!("Error: {}", error.to_string())),
     }
 }
