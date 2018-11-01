040a6bd342408dc87688b5e6c39b36665ab76063
sts10 <sschlinkert@gmail.com>
2018-10-31 21:35:07 -0400

    initial commit

---
+++ src/eyeoh.rs
@@ -0,0 +1,9 @@
+use std::io;
+
+pub fn gets() -> Result<String, String> {
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(_n) => Ok(input.trim().to_string()),
+        Err(error) => Err(format!("Error: {}", error.to_string())),
+    }
+}
