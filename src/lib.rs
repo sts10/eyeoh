dfd3a16e54609cbbabbbcec0d2b80c29215db992
sts10 <sschlinkert@gmail.com>
2018-11-01 07:43:33 -0400

    better result and error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 use std::io;
 
-pub fn gets() -> Result<String, String> {
+pub fn gets() -> io::Result<String> {
     let mut input = String::new();
     match io::stdin().read_line(&mut input) {
         Ok(_n) => Ok(input.trim_end_matches("\n").to_string()),
-        Err(error) => Err(format!("Error: {}", error.to_string())),
+        Err(error) => Err(error),
     }
 }
