5c49dc9271f63d7fa546470cbe12cbb9e97e3cf0
sts10 <sschlinkert@gmail.com>
2018-11-02 07:30:40 -0400

    adds ensure function

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,36 @@
 use std::io;
+use std::str::FromStr;
 
 pub fn gets() -> io::Result<String> {
     let mut input = String::new();
     match io::stdin().read_line(&mut input) {
         Ok(_n) => Ok(input.trim_end_matches("\n").to_string()),
         Err(error) => Err(error),
+    }
+}
+
+pub fn ensure<T: FromStr>(try_again: &str) -> io::Result<T> {
+    loop {
+        let line = match gets() {
+            Ok(l) => l,
+            Err(e) => return Err(e),
+        };
+        // alternatively
+        // let line = gets()?;
+
+        match line.parse() {
+            Ok(res) => return Ok(res),
+            // otherwise, display inputted "try again" message
+            // and continue the loop
+            Err(_e) => {
+                eprintln!("{}", try_again);
+                continue;
+            }
+        };
+
+        // alternatively
+        // if let Ok(res) = line.parse() { return res; }
+        // eprintln!("{}", try_again);
+        // and then let the loop cycle
     }
 }
